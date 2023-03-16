8d4fffd6d9c1f54b4330ad86a2267b6dea48fa0e
Roz Hall <rozbecoz@gmail.com>
2023-03-16 00:46:33 -0700

    Add files via upload
    3-15 CW

---
+++ 8-lost-without-a-map.js
@@ -0,0 +1,43 @@
+//LOST WITHOUT A MAP
+
+/*Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]*/
+
+// Parameters: Edge cases?  Empty arrays?  non-integers?
+// Return: New array with original values doubled.
+// Example cases: [] = []; 
+    //[2, 9, 15] = [4, 18, 30]; 
+    //[ 5, &, 8, , 62] = [10,16,,124]
+//Psuedo: Make function that takes in an array and creates a new array with those values doubled.  
+    //likely use Map.
+
+
+    function maps(array1) {
+        return array1.map(element => element *=2);
+    }
+//mapped array and then multiplied elements by 2 and return new array
+
+
+console.log(maps([2,9,15]),[4,18,30]);
+
+
+//now to account for the edge cases
+
+function maps2(array1) {
+    if(array1.length === 0) {
+        console.log('Please enter an array of integers');
+    } 
+    else { 
+        return array1.map(element => element *=2);
+    }
+}
+
+
+console.log(maps2([4,18,30]), [8,36,60]);
+console.log(maps2([]),[]);
+console.log(maps2([ 5, , 8, , 62]),[10,16,124]);
+//console.log(maps2(['puppy']),[10,16,124]);
+
